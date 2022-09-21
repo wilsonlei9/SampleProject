889042edd498e9d77aa105f317145e5c162de1ec
Wilson <wlei6251@bths.edu>
2022-09-20 21:54:07 -0400

    Added favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,6 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Wilson");
         System.out.println("none");
+        System.out.println("Noodles");
     }
-
 }
